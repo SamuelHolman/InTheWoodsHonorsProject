9c61da050ae638972aa7de1a5616058ecf6c272e
Samuel <sholman@gus.pittstate.edu>
2022-05-03 16:12:23 -0500

    Added lighter caves

--- models/userSchema.js
+++ models/userSchema.js
@@ -21,54 +21,54 @@ const userSchema = new mongoose.Schema(
                 Right: { type: Boolean, default: true }
             },
             Bug: {
-                Dragonfly: { type: Boolean, default: false },                   //
-                Beetle: { type: Boolean, default: false },                      //
-                Snail: { type: Boolean, default: false },                       //
-                Caterpillar: { type: Boolean, default: false },                 //
-                Skater: { type: Boolean, default: false },                      //
-                Mosquito: { type: Boolean, default: false },                    //
-                Antlion: { type: Boolean, default: false },                     //
-                Ant: { type: Boolean, default: false },                         //
-                Bee: { type: Boolean, default: false },                         //
-                Moth: { type: Boolean, default: false },                        //
-                Butterfly: { type: Boolean, default: false },                   //
-                Wasp: { type: Boolean, default: false },                        //
-                Spider: { type: Boolean, default: false },                      //
+                Dragonfly: { type: Boolean, default: false },  
+                Beetle: { type: Boolean, default: false },     
+                Snail: { type: Boolean, default: false },      
+                Caterpillar: { type: Boolean, default: false },
+                Skater: { type: Boolean, default: false },     
+                Mosquito: { type: Boolean, default: false },   
+                Antlion: { type: Boolean, default: false },    
+                Ant: { type: Boolean, default: false },        
+                Bee: { type: Boolean, default: false },        
+                Moth: { type: Boolean, default: false },       
+                Butterfly: { type: Boolean, default: false },  
+                Wasp: { type: Boolean, default: false },       
+                Spider: { type: Boolean, default: false },     
                 Ladybug: { type: Boolean, default: false },
-                Walkingstick: { type: Boolean, default: false },                //
-                ScarabBeetle: { type: Boolean, default: false },                //
-                StinkBug: { type: Boolean, default: false },                    //
-                Mantis: { type: Boolean, default: false },                      //
-                FlowerMantisRed: { type: Boolean, default: false },             //
-                FlowerMantisWhite: { type: Boolean, default: false },           //
-                FlowerMantisYellow: { type: Boolean, default: false },          //
-                Crawdad: { type: Boolean, default: false },                     //
-                Termite: { type: Boolean, default: false },                     //
-                Cockroach: { type: Boolean, default: false },                   //
-                Worm: { type: Boolean, default: false },                        //
-                Skink: { type: Boolean, default: false },                       //
-                Newt: { type: Boolean, default: false },                        //
-                Frog: { type: Boolean, default: false },                        //
-                Salamander: { type: Boolean, default: false },                  //
-                Turtle: { type: Boolean, default: false },                      //
-                Hummingbird: { type: Boolean, default: false },                 //
-                Songbird: { type: Boolean, default: false },                    //
-                TropicalFish: { type: Boolean, default: false },                //
-                Scorpion: { type: Boolean, default: false },                    //
-                MythicalScarabBeetle: { type: Boolean, default: false },        //
-                Amoeba: { type: Boolean, default: false },                      //Frozen
-                LightningBug: { type: Boolean, default: false },                //
-                Firefly: { type: Boolean, default: false },                     //
-                Slug: { type: Boolean, default: false },                        //
-                JumpingSpider: { type: Boolean, default: false },               //
-                BronzeMantis: { type: Boolean, default: false },                //
-                Fly: { type: Boolean, default: false },                         //
-                HermitCrab: { type: Boolean, default: false },                  //
-                Mudskip: { type: Boolean, default: false },                     //
-                Lobster: { type: Boolean, default: false },                     //
-                Lizard: { type: Boolean, default: false },                      //
-                Anglerfish: { type: Boolean, default: false },                  //
-                Stingray: { type: Boolean, default: false },                    //
+                Walkingstick: { type: Boolean, default: false },                
+                ScarabBeetle: { type: Boolean, default: false },                
+                StinkBug: { type: Boolean, default: false },                    
+                Mantis: { type: Boolean, default: false },                      
+                FlowerMantisRed: { type: Boolean, default: false },             
+                FlowerMantisWhite: { type: Boolean, default: false },           
+                FlowerMantisYellow: { type: Boolean, default: false },          
+                Crawdad: { type: Boolean, default: false },                     
+                Termite: { type: Boolean, default: false },                     
+                Cockroach: { type: Boolean, default: false },                   
+                Worm: { type: Boolean, default: false },                        
+                Skink: { type: Boolean, default: false },                       
+                Newt: { type: Boolean, default: false },                        
+                Frog: { type: Boolean, default: false },                        
+                Salamander: { type: Boolean, default: false },                  
+                Turtle: { type: Boolean, default: false },                      
+                Hummingbird: { type: Boolean, default: false },                 
+                Songbird: { type: Boolean, default: false },                    
+                TropicalFish: { type: Boolean, default: false },                
+                Scorpion: { type: Boolean, default: false },                    
+                MythicalScarabBeetle: { type: Boolean, default: false },        
+                Amoeba: { type: Boolean, default: false },
+                LightningBug: { type: Boolean, default: false },                
+                Firefly: { type: Boolean, default: false },                     
+                Slug: { type: Boolean, default: false },                        
+                JumpingSpider: { type: Boolean, default: false },               
+                BronzeMantis: { type: Boolean, default: false },                
+                Fly: { type: Boolean, default: false },                         
+                HermitCrab: { type: Boolean, default: false },                  
+                Mudskip: { type: Boolean, default: false },                     
+                Lobster: { type: Boolean, default: false },                     
+                Lizard: { type: Boolean, default: false },                      
+                Anglerfish: { type: Boolean, default: false },                  
+                Stingray: { type: Boolean, default: false },
                 Millipede: { type: Boolean, default: false }
             }
         }
